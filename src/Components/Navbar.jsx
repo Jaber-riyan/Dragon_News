b1a5b43c32837d07fb49e7d56f40b1c1b6d6a619
Jaber <jaberriyan357@gmail.com>
2024-11-14 23:57:15 +0600

    navbar complete

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+import userLogo from '../assets/user.png';
+
+const Navbar = () => {
+    return (
+        <div className="flex justify-between items-center">
+            <div className=""></div>
+            <div className="space-x-4">
+                <NavLink  to='/' className={({ isActive }) =>
+                    isActive ? "text-[#706F6F] font-black" : "text-[#706F6F] font-semibold"}>Home</NavLink>
+                <NavLink to={'/about'} className={({ isActive }) =>
+                    isActive ? "text-[#706F6F] font-black" : "text-[#706F6F] font-semibold"}>About</NavLink>
+                <NavLink to={'/career'} className={({ isActive }) =>
+                    isActive ? "text-[#706F6F] font-black" : "text-[#706F6F] font-semibold"}>Career</NavLink>
+            </div>
+            <div className="flex justify-center items-center gap-4">
+                <img src={userLogo} alt="" />
+                <button className="px-7 font-semibold py-2 bg-[#403F3F] text-white">Login</button>
+            </div>
+        </div >
+    );
+};
+
+export default Navbar;
--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -1,15 +1,19 @@
 import Header from "../Components/Header";
 import LatestNews from "../Components/LatestNews";
+import Navbar from "../Components/Navbar";
 
 const HomeLayout = () => {
     return (
-        <div>
+        <div className="poppins-font">
             <header>
                 <Header></Header>
                 <section className="w-10/12 mx-auto">
                     <LatestNews></LatestNews>
                 </section>
             </header>
+            <nav className="w-10/12 mx-auto mt-7">
+                <Navbar></Navbar>
+            </nav>
         </div>
     );
 };
